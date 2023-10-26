0713d1ca565ae9e35f45569a94f7a480ddab2936
luizcst <105492527+luizcst@users.noreply.github.com>
2023-10-26 15:47:51 -0300

    ok

--- main.js
+++ main.js
@@ -14,23 +14,23 @@ function tocaSomClap(){
          document.querySelector('#som_tecla_puff').play();
 }
          document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-         function tocaSomClap(){
+         function tocaSomPuff(){
             document.querySelector('#som_tecla_splash').play();
             }
             document.querySelector('.tecla_splash').onclick = tocaSomSplash;
-            function tocaSomClap(){
+            function tocaSomSplash(){
                document.querySelector('#som_tecla_toim').play();
                }
                document.querySelector('.tecla_toim').onclick = tocaSomToim;
-               function tocaSomClap(){
+               function tocaSomToim(){
                   document.querySelector('#som_tecla_psh').play();
                   }
                   document.querySelector('.tecla_psh').onclick = tocaSomPsh;
-                  function tocaSomClap(){
+                  function tocaSomPsh(){
                      document.querySelector('#som_tecla_tic').play();
                      }
                      document.querySelector('.tecla_tic').onclick = tocaSomTic;
-                     function tocaSomClap(){
+                     function tocaSomTic(){
                         document.querySelector('#som_tecla_tom').play();
                         }
                         document.querySelector('.tecla_tom').onclick = tocaSomTom;
