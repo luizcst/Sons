bedd90f14933ed9957e7634617948dc0471b8204
luizcst <105492527+luizcst@users.noreply.github.com>
2023-10-26 15:50:13 -0300

    ok

--- main.js
+++ main.js
@@ -34,6 +34,5 @@ function tocaSomClap(){
                         document.querySelector('#som_tecla_tom').play();
                         }
                         document.querySelector('.tecla_tom').onclick = tocaSomTom;
-
-
-
+                        function tocaSomTom(){
+                        }
