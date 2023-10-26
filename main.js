22dcae9ea3e4a3cd0e22720e69a7bebbc3284b99
luizcst <105492527+luizcst@users.noreply.github.com>
2023-10-26 15:44:21 -0300

    ok

--- main.js
+++ main.js
@@ -9,31 +9,31 @@ function tocaSomClap(){
    function tocaSomClap(){
       document.querySelector('#som_tecla_tim').play();
 }
-      document.querySelector('.tecla_tim').onclick = tocaSomClap;
+      document.querySelector('.tecla_tim').onclick = tocaSomTim;
       function tocaSomClap(){
          document.querySelector('#som_tecla_puff').play();
 }
-         document.querySelector('.tecla_puff').onclick = tocaSomClap;
+         document.querySelector('.tecla_puff').onclick = tocaSomPuff;
          function tocaSomClap(){
             document.querySelector('#som_tecla_splash').play();
             }
-            document.querySelector('.tecla_splash').onclick = tocaSomClap;
+            document.querySelector('.tecla_splash').onclick = tocaSomSplash;
             function tocaSomClap(){
                document.querySelector('#som_tecla_toim').play();
                }
-               document.querySelector('.tecla_toim').onclick = tocaSomClap;
+               document.querySelector('.tecla_toim').onclick = tocaSomToim;
                function tocaSomClap(){
                   document.querySelector('#som_tecla_psh').play();
                   }
-                  document.querySelector('.tecla_psh').onclick = tocaSomClap;
+                  document.querySelector('.tecla_psh').onclick = tocaSomPsh;
                   function tocaSomClap(){
                      document.querySelector('#som_tecla_tic').play();
                      }
-                     document.querySelector('.tecla_tic').onclick = tocaSomClap;
+                     document.querySelector('.tecla_tic').onclick = tocaSomTic;
                      function tocaSomClap(){
                         document.querySelector('#som_tecla_tom').play();
                         }
-                        document.querySelector('.tecla_tom').onclick = tocaSomClap;
+                        document.querySelector('.tecla_tom').onclick = tocaSomTom;
 
 
 
